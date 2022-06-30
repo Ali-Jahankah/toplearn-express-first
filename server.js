0d6e92ea23858ef9720eeb1ca7cd4e92831f1118
Ali Jahankah <aral2@yahoo.com>
2022-06-30 18:28:36 +0100

    learning using static for public files

--- server.js
+++ server.js
@@ -9,15 +9,17 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/home", homeRoutes);
 app.use(adminRoutes);
 app.get("/form", (req, res) => {
   res.sendFile(path.join(rootDir, "pages", "form.html"));
 });
 
-app.use("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.sendFile(path.join(rootDir, "pages", "home.html"));
 });
-
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "404.html"));
+});
 app.listen(4000, () => console.log(`App is running on ${PORT}`));
