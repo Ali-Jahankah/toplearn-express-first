5196c596cdd0ccfd54f97f3a035ceaf64929582a
Ali Jahankah <aral2@yahoo.com>
2022-06-30 17:53:28 +0100

    learning sending pages

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const homeRoutes = require("./routes/home");
 const adminRoutes = require("./routes/admin");
 const bodyParser = require("body-parser");
+const rootDir = require("./utils/paths");
+const path = require("path");
 
 const app = express();
 
@@ -10,16 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/home", homeRoutes);
 app.use(adminRoutes);
-
 app.get("/form", (req, res) => {
-  res.send(
-    "<form action='info' method='POST'><input type='text' name='info'><button type='submit'>Send</button></form>"
-  );
+  res.sendFile(path.join(rootDir, "pages", "form.html"));
 });
 
 app.use("/", (req, res, next) => {
-  console.log("First Middleware...");
-  res.send("Home Page");
+  res.sendFile(path.join(rootDir, "pages", "home.html"));
 });
 
 app.listen(4000, () => console.log(`App is running on ${PORT}`));
