823fe00bb56245b993718becde244849387d069c
Ali Jahankah <aral2@yahoo.com>
2022-08-08 20:52:48 +0100

    Learning handlebars

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ const adminRoutes = require("./routes/admin");
 const bodyParser = require("body-parser");
 const rootDir = require("./utils/paths");
 const path = require("path");
-
+const exphbs = require("express-handlebars");
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 4000;
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set("view engine", "pug");
+// app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    layoutsDir: "views/layouts",
+    defaultLayout: "main",
+    partialDir: "views/partials",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "views");
-app.use(express.static(path.join(__dirname, "public")));
 app.use("/home", homeRoutes);
 app.use(adminRoutes);
 app.get("/form", (req, res) => {
@@ -23,7 +33,11 @@ app.get("/", (req, res, next) => {
 });
 app.get("/pug/index", (req, res) => {
   const arr = [1, 2, 3, 4, 5];
-  res.render("index", { title: "Pug Index", arr: arr });
+  res.render("index", {
+    title: "Pug Index",
+    arr: arr,
+    handlebarsTitle: "This is Handle Bars",
+  });
 });
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "pages", "404.html"));
